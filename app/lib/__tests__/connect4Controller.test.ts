de4706707f97de2ac5c9b2bcc1ca2ae7e947cb8f
Stefania Rincu <Stefania.Rincu@softwire.com>
2026-07-24 10:32:38 +0300

    Re-added code that was accidentally removed

--- app/lib/__tests__/connect4Controller.test.ts
+++ app/lib/__tests__/connect4Controller.test.ts
@@ -78,4 +78,100 @@ describe("Connect4Controller", () => {
       expect(status?.currentPlayer).toBe(2);
     });
   });
+
+  describe("win and draw condition", () => {
+    it("should detect a horizontal win", () => {
+      const controller = new Connect4Controller(7, 6);
+      controller.newGame();
+
+      controller.makeMove(0);
+      controller.makeMove(0);
+      controller.makeMove(1);
+      controller.makeMove(1);
+      controller.makeMove(2);
+      controller.makeMove(2);
+      const status = controller.makeMove(3);
+
+      expect(status?.state).toBe("won");
+      expect(status?.winner).toBe(1);
+    });
+
+    it("should detect a vertical win", () => {
+      const controller = new Connect4Controller(7, 6);
+      controller.newGame();
+
+      controller.makeMove(0);
+      controller.makeMove(1);
+      controller.makeMove(0);
+      controller.makeMove(1);
+      controller.makeMove(0);
+      controller.makeMove(1);
+      const status = controller.makeMove(0);
+
+      expect(status?.state).toBe("won");
+      expect(status?.winner).toBe(1);
+    });
+
+    it("should detect a diagonal win (down-right)", () => {
+      const controller = new Connect4Controller(7, 6);
+      controller.newGame();
+
+      controller.makeMove(0);
+      controller.makeMove(1);
+      controller.makeMove(1);
+      controller.makeMove(2);
+      controller.makeMove(2);
+      controller.makeMove(3);
+      controller.makeMove(2);
+      controller.makeMove(3);
+      controller.makeMove(3);
+      controller.makeMove(0);
+      const status = controller.makeMove(3);
+
+      expect(status?.state).toBe("won");
+      expect(status?.winner).toBe(1);
+    });
+
+    it("should not switch the players once the game ended with one player winning", () => {
+      const controller = new Connect4Controller(7, 6);
+      controller.newGame();
+
+      controller.makeMove(0);
+      controller.makeMove(1);
+      controller.makeMove(0);
+      controller.makeMove(1);
+      controller.makeMove(0);
+      controller.makeMove(1);
+      const status = controller.makeMove(0);
+
+      expect(status?.currentPlayer).toBe(1);
+      expect(controller.makeMove(2)).toBeNull();
+    });
+
+    it("should detect a draw when the board fills", () => {
+      const controller = new Connect4Controller(2, 2);
+      controller.newGame();
+
+      controller.makeMove(0);
+      controller.makeMove(0);
+      controller.makeMove(1);
+      const status = controller.makeMove(1);
+
+      expect(status?.state).toBe("draw");
+      expect(status?.winner).toBeUndefined();
+    });
+
+    it("should not switch the players once the game ended with draw", () => {
+      const controller = new Connect4Controller(2, 2);
+      controller.newGame();
+
+      controller.makeMove(0);
+      controller.makeMove(0);
+      controller.makeMove(1);
+      const status = controller.makeMove(1);
+
+      expect(status?.currentPlayer).toBe(2);
+      expect(controller.makeMove(2)).toBeNull();
+    });
+  });
 });
