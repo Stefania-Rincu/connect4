a63fa1dda2beabd15aed6b2b1d487a6636d3078f
Stefania Rincu <Stefania.Rincu@softwire.com>
2026-07-24 10:16:26 +0300

    Refactored code to use camelCase standard

--- app/lib/connect4Controller.ts
+++ app/lib/connect4Controller.ts
@@ -41,21 +41,21 @@ export class Connect4Controller {
       return null;
     }
 
-    let empty_row = -1;
+    let emptyRow = -1;
     for (let row = this.height - 1; row >= 0; row--) {
       if (this.board[row][column] === 0) {
-        empty_row = row;
+        emptyRow = row;
         break;
       }
     }
 
-    if (empty_row === -1) {
+    if (emptyRow === -1) {
       return null;
     }
 
-    this.board[empty_row][column] = this.currentPlayer;
+    this.board[emptyRow][column] = this.currentPlayer;
 
-    if (this.checkWin(empty_row, column)) {
+    if (this.checkWin(emptyRow, column)) {
       this.gameState = "won";
       return this.getStatus();
     }
@@ -78,24 +78,24 @@ export class Connect4Controller {
     return this.board[0].every((cell) => cell !== 0);
   }
 
-  private checkWin(current_row: number, current_column: number): boolean {
-    const player = this.board[current_row][current_column];
+  private checkWin(currentRow: number, currentColumn: number): boolean {
+    const player = this.board[currentRow][currentColumn];
     if (player === 0) {
       return false;
     }
 
     const directions = [[0, 1], [1, 0], [1, 1], [1, -1]];
 
-    for (const [dir_row, dir_col] of directions) {
+    for (const [dirRow, dirCol] of directions) {
       let count = 1;
 
       for (const sign of [1, -1]) {
-        let row = current_row + dir_row * sign;
-        let col = current_column + dir_col * sign;
+        let row = currentRow + dirRow * sign;
+        let col = currentColumn + dirCol * sign;
         while (row >= 0 && row < this.height && col >= 0 && col < this.width && this.board[row][col] === player) {
           count++;
-          row += dir_row * sign;
-          col += dir_col * sign;
+          row += dirRow * sign;
+          col += dirCol * sign;
         }
       }
 
